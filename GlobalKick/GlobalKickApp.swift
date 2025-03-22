f0ef0916da6f88b01b99b0c4f0f04b2f98fde35b
ruziniuuuuu <ruziniuuuuu@gmail.com>
2025-03-23 06:57:15 +0800

    Initial Commit

---
+++ GlobalKick/GlobalKickApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalKickApp.swift
+//  GlobalKick
+//
+//  Created by 孺子牛 on 2025/3/23.
+//
+
+import SwiftUI
+
+@main
+struct GlobalKickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GlobalKickTests/GlobalKickTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalKickTests.swift
+//  GlobalKickTests
+//
+//  Created by 孺子牛 on 2025/3/23.
+//
+
+import Testing
+@testable import GlobalKick
+
+struct GlobalKickTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
